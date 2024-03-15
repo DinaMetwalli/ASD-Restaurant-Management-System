c975fe8893b9190c5cae5a2eae1382800be56836
Dina Metwalli <dina.moh657@gmail.com>
2024-03-15 17:56:30 +0000

    style(api): fix variable names

--- src/api/routes/branches/[branch_id]/orders/index.py
+++ src/api/routes/branches/[branch_id]/orders/index.py
@@ -2,7 +2,7 @@ from flask import render_template
 
 from src.api.middleware.auth import auth_cleanup, perm_guard
 from src.api.utils.Result import Error, OK, Status
-from src.api.utils.dictify import dictify_simple_order
+from src.api.utils.dictify import dictify_order
 from src.branch.BranchService import BranchService
 from src.order.OrderService import OrderService
 
@@ -20,9 +20,9 @@ def post(branch_id: str = ""):
 
     orders = OrderService.get_all_placed_from_branch(branch)
 
-    inventory_data = [dictify_simple_order(o) for o in orders]
+    order_data = [dictify_order(o) for o in orders]
 
-    return OK({"orders": inventory_data})
+    return OK({"orders": order_data})
 
 
 def get(branch_id: str = ""):
