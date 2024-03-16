74c8c5193581e67d878335793b0c79efbe8a9ed6
Dina Metwalli <dina.moh657@gmail.com>
2024-03-16 01:20:59 +0000

    feat(api): add set event time endpoints

---
+++ src/api/routes/branches/[branch_id]/events/[event_id]/set/start_time.py
@@ -0,0 +1,37 @@
+from marshmallow import Schema, fields
+
+from datetime import datetime
+from src.api.middleware.auth import auth_cleanup, perm_guard
+from src.api.utils.Result import OK, Error, Status
+from src.api.utils.dictify import dictify_event
+from src.branch.BranchService import BranchService
+from src.utils.errors import InputError
+
+guard = perm_guard("event.update.start_time")
+cleanup = auth_cleanup
+
+
+class PostSchema(Schema):
+    start_time = fields.Integer(required=True)
+
+
+def post(body: dict, event_id: str = "", branch_id: str = ""):
+    
+    start_time = body["start_timestamp"]
+    start_datetime_obj = datetime.fromtimestamp(start_time)
+
+    try:
+        branch = BranchService.get_by_id(branch_id)
+        if branch is None:
+            return Error(Status.NOT_FOUND, "Branch not found")
+        
+        event = branch.events().get_by_id(event_id)
+        if event is None:
+            return Error(Status.NOT_FOUND, "Event not found.")
+
+        event.set_start_time(start_datetime_obj)
+    except InputError as e:
+        return Error(Status.BAD_REQUEST, e.message)
+
+    return OK(dictify_event(event))
+
