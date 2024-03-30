70a13f6e047cd06bda3cacd7ecfe389cb83c4318
Dina Metwalli <dina.moh657@gmail.com>
2024-03-30 01:10:13 +0000

    style(gui): update file name for home page

---
+++ gui/Home.py
@@ -0,0 +1,21 @@
+# Author: Dina Hassanein (22066792)
+from gui_lib import Page
+import tkinter as tk
+import customtkinter as ctk
+from tkinter import *
+from tkinter import ttk
+
+
+class HomePage(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        
+        self.notebook = self.create_notebook_widget()
+
+    def create_notebook_widget(self):
+
+        label = ctk.CTkLabel(self, text="Main Page")
+        label.pack(padx=20, pady=20)
+
+    def load_records(self):
+        pass
