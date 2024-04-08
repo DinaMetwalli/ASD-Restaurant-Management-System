7b0a4507cfd9edb97c780361c03db58cead2c8fb
Dina Metwalli <dina.moh657@gmail.com>
2024-04-08 19:12:01 +0100

    feat(gui): add styling to Discounts page

--- gui/Discounts.py
+++ gui/Discounts.py
@@ -1,178 +1,206 @@
 # Author: Dina Hassanein (22066792)
-from gui_lib import Page
-import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+from CTkTable import *
+import customtkinter as ctk
+
 from api import API, URL, State
 
 
-class DiscountsPage(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.notebook = self.create_notebook_widget()
+class DiscountsPage(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
-    def create_notebook_widget(self):
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_columnconfigure((2, 3), weight=0)
+        self.grid_rowconfigure((0, 1, 2), weight=1)
+        
+        self.tab_view = DiscountView(master=self, command=self.on_tab_selected)
+        self.tab_view.grid(row=0, column=0, padx=20, pady=20, columnspan=4)
 
-        style = ttk.Style(self)
-        style.configure('lefttab.TNotebook', tabposition='wn')
+    def on_tab_selected(self):
+        selected_tab = self.tab_view.get()
+        
+        if selected_tab == "View Discounts":
+            self.tab_view.load_data()
+        if selected_tab == "Create Discount":
+            self.tab_view.create_msg.configure(text="")
 
-        notebook = ttk.Notebook(self)
-        notebook.pack(fill='both', expand=True)
+class DiscountView(ctk.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-        # create frames
-        self.frame1 = ViewDiscounts(notebook)
-        self.frame2 = CreateDiscount(notebook)
-        self.frame3 = UpdateDiscount(notebook)
+        self.add("View Discounts")
+        self.add("Create Discount")
 
-        notebook.bind("<<NotebookTabChanged>>", self.on_tab_selected)
-        notebook.add(self.frame1, text='View All Discounts')
-        notebook.add(self.frame2, text='Create Discount')
-        notebook.add(self.frame3, text='Update Discount')
+        self.font = ctk.CTkFont(family="Dosis Semibold", size=20)
 
-    def on_tab_selected(self, event):
-        # ref: https://www.homeandlearn.uk/python-database-form-tabs3.html
-        if State.username is None:
-            return
-        selected_tab = event.widget.select()
-        tab_text = event.widget.tab(selected_tab, "text")
+        self.view_discounts()
+        self.create_discount()
 
-        if tab_text == "View All Discounts":
-            self.frame1.load_records()
-        if tab_text == "Create Discount":
-            self.frame1.load_records()
-        if tab_text == "Update Discount":
-            self.frame1.load_records()
-
-
-class ViewDiscounts(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.create_widgets()
-
-    def create_widgets(self):
-        columns = ('multiplier', 'description')
-
-        self.tree = ttk.Treeview(self, columns=columns, show='headings')
-        self.tree.heading('multiplier', text='Multiplier')
-        self.tree.heading('description', text='Description')
-
-        delete_button = ttk.Button(
-            self, text='Delete All Records', command=self.delete_all_records)
-        delete_button.pack(anchor=tk.E, padx=5, pady=5)
-
-    def load_records(self):
+    def load_data(self):
         if State.branch_id is None:
             return
-        for item in self.tree.get_children():
-            self.tree.delete(item)
-        branch_id = State.branch_id
-        branch_discounts_res = API.post(
-            f"{URL}/branches/{branch_id}/discounts")
-        branch_discounts = branch_discounts_res.json()
+        
+        value = [["Discount Name", "Percentage", "Description"]]
 
+        for i in range(1, len(self.table.values)):
+            if len(self.table.values) > 1:
+                self.table.delete_row(index=i)
+
+        branch_id = State.branch_id
+        all_discounts_res = API.post(
+            f"{URL}/branches/{branch_id}/discounts")
+        all_discounts = all_discounts_res.json()
+        
         global discount_data
         discount_data = {}
 
-        for discount in branch_discounts["data"]["discounts"]:
-            self.tree.insert('', 'end', values=(
-                discount["multiplier"], discount["description"]))
-            discount_data[discount["multiplier"]] = discount["id"]
+        for discount in all_discounts["data"]["discounts"]:
+            value.append([discount["name"], discount["multiplier"],
+                          discount["description"]])
+            data = {discount["name"]: discount["id"]}
+            discount_data.update(data)
 
-        self.tree.pack(fill='x', expand=True)
+        print(value)
+        for i in range(0, len(value)):
+            self.table.add_row(index=i, values=value[i])
+        
+        self.table.delete_row(index=-1)
+
+    def on_press(self, data):
+        self.discount = data["value"]
+        if self.discount != "Discount Name":
+            self.update_button.configure(state="normal")
+            self.delete_button.configure(state="normal")
+        else:
+            self.update_button.configure(state="disabled")
+            self.delete_button.configure(state="disabled")
+
+    def view_discounts(self):
+
+        self.tab("View Discounts").columnconfigure((1, 2), minsize=1000)
+        self.tab("View Discounts").rowconfigure(2, minsize=245)
+
+        self.label = ctk.CTkLabel(master=self.tab("View Discounts"), text="View all discounts",
+                                  font=self.font)
+        self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
+        
+        self.scrollable_frame = ctk.CTkScrollableFrame(master=self.tab("View Discounts"),
+                                                       width=720, height=350)
+        self.scrollable_frame.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame.grid_columnconfigure(0, weight=1)
+
+        self.value = [["Discount Name", "Percentage", "Description"]]
+        self.table = CTkTable(master=self.scrollable_frame, column=3,
+                              row=1, hover=True, command=self.on_press,
+                              values=self.value)
+        self.table.grid(row=0, column=0, padx=0, pady=20, sticky="ew")
+
+        self.load_data()
+
+        self.view_frame = ctk.CTkFrame(master=self.tab("View Discounts"),
+                                       fg_color="#333333")
+        self.view_frame.grid(row=2, column=0, sticky="sew", pady=(0,10), padx=10)
+
+        self.update_button = ctk.CTkButton(master=self.view_frame,
+                                      text='Update Discount', command= lambda:
+                                      self.configure_update(self.discount))
+
+        self.delete_button = ctk.CTkButton(master=self.view_frame,
+                                      text='Delete Discount', command= lambda:
+                                      self.delete_record(self.discount))
+        self.delete_button.grid(row=0, column=0, padx=10)
+        self.delete_button.configure(state="disabled")
+
+        self.delete_all_button = ctk.CTkButton(master=self.view_frame,
+                                      text='Delete All Records', command=
+                                      self.delete_all_records)
+        self.delete_all_button.grid(row=0, column=1, padx=10)
+
+    def delete_record(self, discount):
+        discount_id = discount_data[discount]
+        branch_id = State.branch_id
+        
+        API.post(f"{URL}/branches/{branch_id}/discounts/{discount_id}/delete")
+        
+        self.load_data()
+
+        self.update_button.configure(state="disabled")
+        self.delete_button.configure(state="disabled")
 
     def delete_all_records(self):
+        discounts_ids = []
         branch_id = State.branch_id
+        
+        if branch_id is None:
+            return
+        
         for record in discount_data:
-            res = API.post(
-                f"{URL}/branches/{branch_id}/discounts/{discount_data[record]}/delete")
-        self.load_records()
+            discounts_ids.append(discount_data[record])
+        for id in discounts_ids:
+            API.post(f"{URL}/branches/{branch_id}/discounts/{id}/delete")
+        
+        self.load_data()
+        
+        self.update_button.configure(state="disabled")
+        self.delete_button.configure(state="disabled")
 
+    def create_discount(self):
+        self.discount_name = ctk.StringVar()
+        self.multiplier = ctk.DoubleVar()
+        self.description = ctk.StringVar()
 
-class CreateDiscount(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.create_widgets()
+        self.tab("Create Discount").columnconfigure((0, 1, 2), minsize=1000)
+        self.tab("Create Discount").rowconfigure(5, minsize=585)
 
-    def create_widgets(self):
-        self.multipler = tk.DoubleVar()
-        self.description = tk.StringVar()
+        self.label = ctk.CTkLabel(master=self.tab("Create Discount"), text="Create a discount",
+                                  font=self.font)
+        self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 
-        self.fields = {}
+        self.discount_label = ctk.CTkLabel(master=self.tab("Create Discount"), text='Discount Name')
+        self.discount_label.grid(row=1, column=0, padx=20, pady=5, sticky="nw")
 
-        self.fields['item_name_label'] = ttk.Label(self, text='Multiplier:')
-        self.fields['item_name'] = ttk.Entry(self, textvariable=self.multipler)
-        self.fields['description_label'] = ttk.Label(self, text='Description:')
-        self.fields['description'] = ttk.Entry(
-            self, textvariable=self.description)
+        self.discount_entry = ctk.CTkEntry(master=self.tab("Create Discount"), textvariable=
+                                       self.discount_name, width=350)
+        self.discount_entry.grid(row=1, column=0, padx=(150,0), pady=5, sticky="nw")
 
-        for field in self.fields.values():
-            field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+        self.multiplier_label = ctk.CTkLabel(master=self.tab("Create Discount"), text='Multiplier')
+        self.multiplier_label.grid(row=2, column=0, padx=20, pady=5, sticky="nw")
 
-        self.fields['message'] = ttk.Label(self, text="", wraplength=370)
-        self.fields['message'].pack(
-            anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+        self.multiplier_entry = ctk.CTkEntry(master=self.tab("Create Discount"), textvariable=
+                                       self.multiplier, width=350)
+        self.multiplier_entry.grid(row=2, column=0, padx=(150,0), pady=5, sticky="nw")
 
-        create_button = ttk.Button(
-            self, text='Create Discount ', command=self.add_record)
-        create_button.pack(anchor=tk.E, padx=5, pady=5)
+        self.desc_label = ctk.CTkLabel(master=self.tab("Create Discount"), text='Description')
+        self.desc_label.grid(row=3, column=0, padx=20, pady=5, sticky="nw")
+
+        self.desc_entry = ctk.CTkEntry(master=self.tab("Create Discount"), textvariable=
+                                       self.description, width=350)
+        self.desc_entry.grid(row=3, column=0, padx=(150,0), pady=5, sticky="nw")
+
+        self.create_msg = ctk.CTkLabel(master=self.tab("Create Discount"), text="")
+        self.create_msg.grid(row=4, column=0, padx=(150,0), pady=5, sticky="nw")
+
+        self.create_frame = ctk.CTkFrame(master=self.tab("Create Discount"), fg_color="#333333")
+        self.create_frame.grid(row=5, column=0, sticky="sew", pady=(0,10), padx=10)
+
+        create_button = ctk.CTkButton(master=self.create_frame,
+                                      text='Create Discount',
+                                      command=self.add_record)
+        create_button.grid(padx=10, row=0, column=0)
 
     def add_record(self):
-        for multiplier in discount_data:
-            if multiplier == self.multipler.get():
-                self.fields['message']["text"] = "This Discount Already Exists."
-                return
         branch_id = State.branch_id
-        if branch_id is None:
-            self.fields['message']["text"] = "Current user isn't logged into or assigned a branch, Choose a branch on login first to create branch items"
-            return
-        self.fields['message']["text"] = ""
-        discount_info = {"multiplier": self.multipler.get(
-        ), "description": self.description.get()}
-        branch_id = State.branch_id
-        create = API.post(
-            f"{URL}/branches/{branch_id}/discounts/create", json=discount_info)
+        name = self.discount_name.get()
+        multiplier = self.multiplier.get()
+        desc = self.description.get()
+
+        discount_data = {"name": name, "multiplier": multiplier, "description": desc}
+        print(discount_data)
+        create = API.post(f"{URL}/branches/{branch_id}/discounts/create", json=discount_data)
         match create.status_code:
             case 200:
-                self.fields['message']["text"] = "Discount Created Successfully"
-            case 400 | 409 | 401:
-                self.fields['message']["text"] = create.json()["message"]
-
-
-class UpdateDiscount(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.multiplier = tk.DoubleVar()
-
-        self.fields = {}
-
-        self.fields['item_name_label'] = ttk.Label(
-            self, text='Find Discount By Multiplier:')
-        self.fields['item_name'] = ttk.Entry(
-            self, textvariable=self.multiplier)
-
-        for field in self.fields.values():
-            field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
-
-        self.fields['message'] = ttk.Label(self, text="")
-        self.fields['message'].pack(
-            anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
-
-        delete_button = ttk.Button(
-            self, text='Delete Discount ', command=self.delete_record)
-        delete_button.pack(anchor=tk.E, padx=5, pady=5)
-
-    def delete_record(self):
-        multiplier = self.multiplier.get()
-        multiplier_id = discount_data[multiplier]
-        branch_id = State.branch_id
-        for record in discount_data:
-            if multiplier == record:
-                delete_item = API.post(
-                    f"{URL}/branches/{branch_id}/discounts/{multiplier_id}/delete")
-                break
-        match delete_item.status_code:
-            case 200:
-                self.fields['message']["text"] = "Discount Deleted Successfully"
+                self.create_msg.configure(text="Discount Created Successfully")
+                self.discount_name.set("")
+            case 400 | 409:
+                self.create_msg.configure(text=f"Invalid Input: {create.json()["message"]}")
