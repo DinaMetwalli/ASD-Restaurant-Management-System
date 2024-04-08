4b23ff641c04c4d1729916b6cdc4b368808fc4d3
Dina Metwalli <dina.moh657@gmail.com>
2024-04-08 12:59:50 +0100

    feat(discounts): add name field to SQL table

--- src/init_sql/5discounts.sql
+++ src/init_sql/5discounts.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS public.discounts(
     id uuid NOT NULL DEFAULT gen_random_uuid() UNIQUE,
+    name text NOT NULL UNIQUE,
     multiplier decimal(3,2),
     description text,    
     branch_id uuid NOT NULL,
