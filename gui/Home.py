d6c64ed0a5ca27f35845beffbdbfdb0bfdfa280b
Dina Metwalli <dina.moh657@gmail.com>
2024-04-11 19:20:05 +0100

    feat(gui): add basic home page

--- gui/Home.py
+++ gui/Home.py
@@ -1,21 +1,31 @@
 # Author: Dina Hassanein (22066792)
-from gui_lib import Page
-import tkinter as tk
 import customtkinter as ctk
 from tkinter import *
-from tkinter import ttk
+
+from PIL import Image
 
 
 class HomePage(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
         
-        self.notebook = self.create_notebook_widget()
+        self.grid_rowconfigure(0, weight=1, minsize=900)
+        self.grid_columnconfigure(0, weight=1)
 
-    def create_notebook_widget(self):
+        self.create_widgets()
+        
+    def create_widgets(self):
+        self.frame = ctk.CTkFrame(master=self)
+        self.frame.grid(row=0, column=0, sticky="nsew",
+                        rowspan=4, columnspan=4)
+        
+        self.frame.grid_columnconfigure(0, weight=1)
+        # self.frame.grid_rowconfigure(0, weight=1)
 
-        label = ctk.CTkLabel(self, text="Main Page")
-        label.pack(padx=20, pady=20)
+        self.inner_frame = ctk.CTkFrame(master=self.frame)
+        self.inner_frame.grid(row=0, column=0, sticky="nsew",
+                              rowspan=4, columnspan=4, padx=10, pady=10)
 
-    def load_records(self):
-        pass
+        image = ctk.CTkImage(Image.open("gui/assets/title_bg.jpg"), size=(530,350))
+        self.image_label = ctk.CTkLabel(self.frame, image=image, text="")
+        self.image_label.grid(row=0, column=0, sticky="n", columnspan=4, pady=12)
