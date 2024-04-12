5bb9e6b4e738f17dd600b90f6f4fe89ed77fca89
Dina Metwalli <dina.moh657@gmail.com>
2024-04-12 19:40:14 +0100

    feat(gui): add styling to Table page

--- gui/Table.py
+++ gui/Table.py
@@ -1,195 +1,270 @@
 # Author: Dina Hassanein (22066792)
-from gui_lib import Page
-import tkinter as tk
 from tkinter import *
-from tkinter import ttk
+from CTkTable import *
+import customtkinter as ctk
+
 from api import API, URL, State
 
 
-class TablesPage(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.notebook = self.create_notebook_widget()
+class TablesPage(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
 
-    def create_notebook_widget(self):
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+        
+        self.tab_view = TableView(master=self, command=self.on_tab_selected)
+        self.tab_view.grid(row=0, column=0, padx=20, pady=20, columnspan=4)
 
-        style = ttk.Style(self)
-        style.configure('lefttab.TNotebook', tabposition='wn')
+    def on_tab_selected(self):
+        selected_tab = self.tab_view.get()
+        
+        if selected_tab == "View Tables":
+            self.tab_view.load_data()
+        if selected_tab == "Create Table":
+            self.tab_view.create_msg.configure(text="")
 
-        notebook = ttk.Notebook(self)
-        notebook.pack(fill='both', expand=True)
+class TableView(ctk.CTkTabview):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-        # create frames
-        self.frame1 = ViewTables(notebook)
-        self.frame2 = CreateTable(notebook)
-        self.frame3 = UpdateTable(notebook)
+        self.add("View Tables")
+        self.add("Create Table")
+        self.add("Update Table")
 
-        notebook.bind("<<NotebookTabChanged>>", self.on_tab_selected)
-        notebook.add(self.frame1, text='View All Tables')
-        notebook.add(self.frame2, text='Create Table')
-        notebook.add(self.frame3, text='Update Table')
+        self.font = ctk.CTkFont(family="Dosis Semibold", size=20)
 
-    def on_tab_selected(self, event):
-        # ref: https://www.homeandlearn.uk/python-database-form-tabs3.html
-        selected_tab = event.widget.select()
-        tab_text = event.widget.tab(selected_tab, "text")
+        self.view_tables()
+        self.create_table()
+        self.update_table()
 
-        if tab_text == "View All Tables":
-            self.frame1.load_records()
-        if tab_text == "Create Table":
-            self.frame2.fields['message']['text'] = ""
-            self.frame1.load_records()
-        if tab_text == "Update Table":
-            self.frame1.load_records()
-            self.frame2.fields['message']['text'] = ""
-
-
-class ViewTables(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.create_widgets()
-
-    def create_widgets(self):
-        columns = ('table_number', 'capacity')
-
-        self.tree = ttk.Treeview(self, columns=columns, show='headings')
-        self.tree.heading('table_number', text='Table Number')
-        self.tree.heading('capacity', text='Capacity')
-
-        delete_button = ttk.Button(
-            self, text='Delete All Records', command=self.delete_all_records)
-        delete_button.pack(anchor=tk.E, padx=5, pady=5)
-
-    def load_records(self):
+    def load_data(self):
         if State.branch_id is None:
             return
-        for item in self.tree.get_children():
-            self.tree.delete(item)
-        branch_id = State.branch_id
-        branch_tables_res = API.post(
-            f"{URL}/branches/{branch_id}/tables")
-        branch_tables = branch_tables_res.json()
+        
+        value = [["Number", "Capacity"]]
 
+        for i in range(1, len(self.table_list.values)):
+            if len(self.table_list.values) > 1:
+                self.table_list.delete_row(index=i)
+
+        branch_id = State.branch_id
+        all_tables_res = API.post(
+            f"{URL}/branches/{branch_id}/tables")
+        all_tables = all_tables_res.json()
+        
         global table_data
         table_data = []
 
-        for table in branch_tables["data"]["tables"]:
-            self.tree.insert('', 'end', values=(
-                table["number"], table["capacity"]))
+        for table in all_tables["data"]["tables"]:
+            value.append([table["number"], table["capacity"]])
             table_data.append(table["number"])
 
-        self.tree.pack(fill='x', expand=True)
+        for i in range(0, len(value)):
+            self.table_list.add_row(index=i, values=value[i])
+        
+        self.table_list.delete_row(index=-1)
+
+    def on_press(self, data):
+        self.table = data["value"]
+        if data["column"] != 0 or data["row"] == 0:
+            self.delete_button.configure(state="disabled")
+            self.update_button.configure(state="disabled")
+        else:
+            self.delete_button.configure(state="normal")
+            self.update_button.configure(state="normal")
+
+    def view_tables(self):
+        self.tab("View Tables").grid_rowconfigure(0, minsize=700)
+
+        window = ctk.CTkFrame(master=self.tab("View Tables"),
+                              fg_color="#333333", height=600)
+        window.grid(row=0, column=0, sticky="n")
+        window.columnconfigure((0, 1, 2), minsize=1000)
+
+        self.label = ctk.CTkLabel(master=window, text="View All Tables",
+                                  font=self.font)
+        self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
+        
+        self.scrollable_frame = ctk.CTkScrollableFrame(master=window,
+                                                       width=720, height=350)
+        self.scrollable_frame.grid(row=1, column=0, padx=(45, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame.grid_columnconfigure(0, weight=1)
+
+        self.value = [["Number", "Capacity"]]
+        self.table_list = CTkTable(master=self.scrollable_frame, column=2,
+                              row=1, hover=True, command=self.on_press,
+                              values=self.value)
+        self.table_list.grid(row=0, column=0, padx=0, pady=20, sticky="ew")
+
+        self.load_data()
+
+        self.view_frame = ctk.CTkFrame(master=self.tab("View Tables"),
+                                       fg_color="#333333")
+        self.view_frame.grid(row=2, column=0, sticky="sew")
+
+        self.update_button = ctk.CTkButton(master=self.view_frame,
+                                      text='Update Table', command= lambda:
+                                      self.configure_update(self.table))
+        self.update_button.grid(row=0, column=0, pady=(0,10), padx=10)
+        self.update_button.configure(state="disabled")
+
+        self.delete_button = ctk.CTkButton(master=self.view_frame,
+                                      text='Delete Table', command= lambda:
+                                      self.delete_record(self.table))
+        self.delete_button.grid(row=0, column=1, padx=10, pady=(0,10))
+        self.delete_button.configure(state="disabled")
+
+        self.delete_all_button = ctk.CTkButton(master=self.view_frame,
+                                      text='Delete All Records', command=
+                                      self.delete_all_records)
+        self.delete_all_button.grid(row=0, column=2, padx=10, pady=(0,10))
+
+    def configure_update(self, table):
+        self.updated_table = table
+        self.set("Update Table")
+
+        self.update_label.configure(text=f"Table to be updated: Table No. {self.updated_table}")
+        self.table_update_btn.configure(state="normal")
+        self.new_capacity_entry.configure(state="normal")
+        self.update_msg.configure(text="")
+
+    def delete_record(self, number):
+        branch_id = State.branch_id
+        
+        API.post(f"{URL}/branches/{branch_id}/tables/{number}/delete")
+        
+        self.load_data()
+
+        self.delete_button.configure(state="disabled")
 
     def delete_all_records(self):
         branch_id = State.branch_id
-        for table_number in table_data:
-            res = API.post(
-                f"{URL}/branches/{branch_id}/tables/{table_number}/delete")
-        self.load_records()
+        
+        if branch_id is None:
+            return
+        
+        for number in table_data:
+            API.post(f"{URL}/branches/{branch_id}/tables/{number}/delete")
+        
+        self.load_data()
+        self.delete_button.configure(state="disabled")
 
+    def create_table(self):
+        self.table_number = ctk.IntVar()
+        self.table_capacity = ctk.IntVar()
 
-class CreateTable(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.create_widgets()
+        self.tab("Create Table").grid_rowconfigure(0, minsize=700)
+        
+        window = ctk.CTkFrame(master=self.tab("Create Table"),
+                              fg_color="#333333", height=600)
+        window.grid(row=0, column=0, sticky="n")
+        window.columnconfigure((0, 1, 2), minsize=1000)
 
-    def create_widgets(self):
-        self.table_number = tk.IntVar()
-        self.capacity = tk.IntVar()
+        self.label = ctk.CTkLabel(master=window, text="Create a table",
+                                  font=self.font)
+        self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 
-        self.fields = {}
+        self.table_label = ctk.CTkLabel(master=window, text='Table Number')
+        self.table_label.grid(row=1, column=0, padx=20, pady=5, sticky="nw")
 
-        self.fields['table_number_label'] = ttk.Label(
-            self, text='Table Number:')
-        self.fields['table_number'] = ttk.Entry(
-            self, textvariable=self.table_number)
-        self.fields['capacity_label'] = ttk.Label(self, text='Capacity:')
-        self.fields['capacity'] = ttk.Entry(self, textvariable=self.capacity)
+        self.table_entry = ctk.CTkEntry(master=window, textvariable=
+                                       self.table_number, width=350)
+        self.table_entry.grid(row=1, column=0, padx=(150,0), pady=5, sticky="nw")
 
-        for field in self.fields.values():
-            field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+        self.capacity_label = ctk.CTkLabel(master=window, text='Table Capacity')
+        self.capacity_label.grid(row=2, column=0, padx=20, pady=5, sticky="nw")
 
-        self.fields['message'] = ttk.Label(self, text="")
-        self.fields['message'].pack(
-            anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
+        self.capacity_entry = ctk.CTkEntry(master=window, textvariable=
+                                       self.table_capacity, width=350)
+        self.capacity_entry.grid(row=2, column=0, padx=(150,0), pady=5, sticky="nw")
 
-        create_button = ttk.Button(
-            self, text='Create Table', command=self.add_record)
-        create_button.pack(anchor=tk.E, padx=5, pady=5)
+        self.create_msg = ctk.CTkLabel(master=window, text="")
+        self.create_msg.grid(row=3, column=0, padx=(150,0), pady=5, sticky="nw")
+
+        self.create_frame = ctk.CTkFrame(master=self.tab("Create Table"),
+                                         fg_color="#333333")
+        self.create_frame.grid(row=1, column=0, sticky="sew")
+
+        create_button = ctk.CTkButton(master=self.create_frame,
+                                      text='Create Table',
+                                      command=self.add_record)
+        create_button.grid(padx=10, pady=(0,10), row=0, column=0)
 
     def add_record(self):
         branch_id = State.branch_id
         if branch_id is None:
-            self.fields['message']["text"] = "Current user isn't logged into or assigned a branch, Choose a branch on login first to create branch items"
-            return
+            self.create_msg.configure(text="User must be logged into a branch"
+                                      " first before editing branch data.")
+        
         number = self.table_number.get()
-        capacity = self.capacity.get()
-        for table_number in table_data:
-            if table_number == number:
-                self.fields['message']["text"] = "A table with this number already exists."
-                return
-        if number != "":
-            if number != 0:
-                if capacity != 0:
-                    table_info = {"table_number": number, "capacity": capacity}
-                    create = API.post(
-                        f"{URL}/branches/{branch_id}/tables/create", json=table_info)
-                    match create.status_code:
-                        case 200:
-                            self.fields['message']["text"] = "Branch Table Created Successfully"
-                        case 400 | 409 | 401:
-                            self.fields['message']["text"] = create.json()[
-                                "message"]
-                else:
-                    self.fields['message']["text"] = "Table capacity cannot be 0."
-                    return
-            else:
-                self.fields['message']["text"] = "Table number cannot be 0."
-                return
-        else:
-            self.fields['message']["text"] = "Please enter a table number."
-            return
+        capacity = self.table_capacity.get()
 
-
-class UpdateTable(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        Page.__init__(self, *args, **kwargs)
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.table_number = tk.IntVar()
-
-        self.fields = {}
-        self.fields['tab_title'] = ttk.Label(
-            self, text="Update Table Information")
-        self.fields['find_table'] = ttk.Label(
-            self, text='Find Table by Table Number:')
-        self.fields['table_number'] = ttk.Entry(
-            self, textvariable=self.table_number)
-
-        for field in self.fields.values():
-            field.pack(anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
-
-        self.fields['message'] = ttk.Label(self, text="")
-        self.fields['message'].pack(
-            anchor=tk.W, padx=10, pady=5, fill=tk.X, expand=True)
-        delete_button = ttk.Button(
-            self, text='Delete Table', command=self.delete_record)
-        delete_button.pack(anchor=tk.E, padx=5, pady=5)
-
-    def delete_record(self):
-        number = self.table_number.get()
-        branch_id = State.branch_id
-        found = False
-        for table_number in table_data:
-            if table_number == number:
-                found = True
-                delete_item = API.post(
-                    f"{URL}/branches/{branch_id}/tables/{number}/delete")
-                break
-        if found == False:
-            self.fields['message']["text"] = "No table exists with this number"
-            return
-        match delete_item.status_code:
+        table_info = {"table_number": number, "capacity": capacity}
+        
+        create = API.post(f"{URL}/branches/{branch_id}/tables/create", json=table_info)
+        match create.status_code:
             case 200:
-                self.fields['message']["text"] = "Branch Table Deleted Successfully"
+                self.create_msg.configure(text="Table Created Successfully")
+                self.table_number.set("")
+                self.table_capacity.set("")
+            case 400 | 409:
+                self.create_msg.configure(text=f"Error: {create.json()["message"]}")
+
+    def update_table(self):
+        self.new_capacity = ctk.IntVar()
+
+        self.tab("Update Table").grid_rowconfigure(0, minsize=700)
+
+        window = ctk.CTkFrame(master=self.tab("Update Table"),
+                              fg_color="#333333", height=600)
+        window.grid(row=0, column=0, sticky="n")
+        window.columnconfigure((0, 1, 2), minsize=1000)
+
+        self.label = ctk.CTkLabel(master=window, text="Update A Table",
+                                  font=self.font)
+        self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
+
+        self.update_label = ctk.CTkLabel(master=window,
+                                      text="Please select a table first")
+        self.update_label.grid(row=1, column=0, padx=20, pady=5, sticky="w")
+        
+        self.new_capacity_label = ctk.CTkLabel(master=window, text="New Table Capacity")
+        self.new_capacity_label.grid(row=2, column=0, padx=20, pady=5, sticky="w")
+
+        self.new_capacity_entry = ctk.CTkEntry(master=window, textvariable=
+                                       self.new_capacity, width=350)
+        self.new_capacity_entry.grid(row=2, column=0, padx=(160,0), pady=5, sticky="w")
+        
+        self.update_msg = ctk.CTkLabel(master=window, text="")
+        self.update_msg.grid(row=3, column=0, padx=(160,0), pady=5, sticky="w")
+
+        self.update_frame = ctk.CTkFrame(master=self.tab("Update Table"), fg_color="#333333")
+        self.update_frame.grid(row=4, column=0, sticky="sew")
+
+        self.table_update_btn = ctk.CTkButton(master=self.update_frame,
+                                      text='Update Table', command=lambda:
+                                      self.update_record(self.updated_table))
+        self.table_update_btn.grid(pady=(0,10), padx=10, row=0, column=0)
+        
+        self.table_update_btn.configure(state="disabled")    
+        self.new_capacity_entry.configure(state="disabled")
+
+    def update_record(self, table_num):
+        branch_id = State.branch_id
+        capacity_data = self.new_capacity_entry.get()
+
+        set_capacity = API.post(
+            f"{URL}/branches/{branch_id}/tables/{table_num}/set/capacity", json={"capacity": capacity_data})
+        
+        match set_capacity.status_code:
+            case 200:
+                self.new_capacity.set("")
+                self.update_msg.configure(text="Table Capacity Set Successfully")
+                self.update_label.configure(text="Please select a table first")
+                self.table_update_btn.configure(state="disabled")
+                self.new_capacity_entry.configure(state="disabled")
+                self.update_button.configure(state="disabled")
+                self.delete_button.configure(state="disabled")
+            case 400 | 409:
+                self.update_msg.configure(text=f"Error: {set_capacity.json()["message"]}")
