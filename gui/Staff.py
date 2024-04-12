1f644c249e26da8d1ea24fe9a27ae4239336a934
Dina Metwalli <dina.moh657@gmail.com>
2024-04-12 19:47:18 +0100

    feat(gui): add styling to Staff page

--- gui/Staff.py
+++ gui/Staff.py
@@ -12,8 +12,7 @@ class StaffPage(ctk.CTkFrame):
         super().__init__(master)
         
         self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((0, 1, 2), weight=1)
+        self.grid_rowconfigure(0, weight=1)
         
         self.tab_view = StaffView(master=self, command=self.on_tab_selected)
         self.tab_view.grid(row=0, column=0, padx=20, pady=20, columnspan=4)
@@ -80,19 +79,22 @@ class StaffView(ctk.CTkTabview):
             self.user = data["value"]
             
         self.updated_staff = self.table.get(data["row"], 0)
-        print(self.updated_staff)
             
     def view_staff(self):
-        self.tab("View Staff").columnconfigure((1, 2), minsize=1000)
-        self.tab("View Staff").rowconfigure(2, minsize=245)
+        self.tab("View Staff").grid_rowconfigure(0, minsize=700)
 
-        self.label = ctk.CTkLabel(master=self.tab("View Staff"), text="View all Branch Staff",
+        window = ctk.CTkFrame(master=self.tab("View Staff"),
+                              fg_color="#333333", height=600)
+        window.grid(row=0, column=0, sticky="n")
+        window.columnconfigure((0, 1, 2), minsize=1000)
+
+        self.label = ctk.CTkLabel(master=window, text="View all Branch Staff",
                                   font=self.font)
         self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
         
-        self.scrollable_frame = ctk.CTkScrollableFrame(master=self.tab("View Staff"),
+        self.scrollable_frame = ctk.CTkScrollableFrame(master=window,
                                                        width=720, height=350)
-        self.scrollable_frame.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
+        self.scrollable_frame.grid(row=1, column=0, padx=(45, 0), pady=(20, 0), sticky="nsew")
         self.scrollable_frame.grid_columnconfigure(0, weight=1)
 
         self.value = [["Username", "Full Name", "Role", "Branch"]]
@@ -106,24 +108,24 @@ class StaffView(ctk.CTkTabview):
 
         self.view_frame = ctk.CTkFrame(master=self.tab("View Staff"),
                                        fg_color="#333333")
-        self.view_frame.grid(row=2, column=0, sticky="sew", pady=(0,10), padx=10)
+        self.view_frame.grid(row=2, column=0, sticky="sew")
 
         self.update_button = ctk.CTkButton(master=self.view_frame,
                                       text='Update Staff', command= lambda:
                                       self.configure_update(self.updated_staff))
-        self.update_button.grid(row=0, column=0, padx=10)
+        self.update_button.grid(row=0, column=0, pady=(0,10), padx=10)
         self.update_button.configure(state="disabled")
 
         self.delete_button = ctk.CTkButton(master=self.view_frame,
                                       text='Delete Staff', command= lambda:
                                       self.delete_record(self.user))
-        self.delete_button.grid(row=0, column=1, padx=10)
+        self.delete_button.grid(row=0, column=1, pady=(0,10), padx=10)
         self.delete_button.configure(state="disabled")
 
         self.delete_all_button = ctk.CTkButton(master=self.view_frame,
                                       text='Delete All Records', command=
                                       self.delete_all_records)
-        self.delete_all_button.grid(row=0, column=2, padx=10)
+        self.delete_all_button.grid(row=0, column=2, pady=(0,10), padx=10)
 
     def configure_update(self, to_update):
         self.set("Update Staff")
@@ -163,55 +165,62 @@ class StaffView(ctk.CTkTabview):
                          "Chef", "Manager", "Admin"]
         self.password = ctk.StringVar()
 
-        self.tab("Create Staff").columnconfigure((0, 1, 2), minsize=1000)
-        self.tab("Create Staff").rowconfigure(5, minsize=385)
+        self.tab("Create Staff").grid_rowconfigure(0, minsize=700)
 
-        self.label = ctk.CTkLabel(master=self.tab("Create Staff"), text="Create a staff member",
+        window = ctk.CTkFrame(master=self.tab("Create Staff"),
+                              fg_color="#333333", height=600)
+        window.grid(row=0, column=0, sticky="n")
+        window.columnconfigure((0, 1, 2), minsize=1000)
+
+        self.label = ctk.CTkLabel(master=window, text="Create a staff member",
                                   font=self.font)
         self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 
-        self.full_name_label = ctk.CTkLabel(master=self.tab("Create Staff"), text='Full Name')
+        self.full_name_label = ctk.CTkLabel(master=window, text='Full Name')
         self.full_name_label.grid(row=1, column=0, padx=20, pady=5, sticky="w")
 
-        self.full_name_entry = ctk.CTkEntry(master=self.tab("Create Staff"), textvariable=
+        self.full_name_entry = ctk.CTkEntry(master=window, textvariable=
                                        self.full_name, width=350)
         self.full_name_entry.grid(row=1, column=0, padx=(150,0), pady=5, sticky="w")
 
-        self.username_label = ctk.CTkLabel(master=self.tab("Create Staff"), text='Username')
+        self.username_label = ctk.CTkLabel(master=window, text='Username')
         self.username_label.grid(row=2, column=0, padx=20, pady=5, sticky="w")
 
-        self.username_entry = ctk.CTkEntry(master=self.tab("Create Staff"), textvariable=
+        self.username_entry = ctk.CTkEntry(master=window, textvariable=
                                        self.username, width=350)
         self.username_entry.grid(row=2, column=0, padx=(150,0), pady=5, sticky="w")
 
-        self.password_label = ctk.CTkLabel(master=self.tab("Create Staff"), text='Password')
-        self.password_label.grid(row=3, column=0, padx=20, pady=(30,10), sticky="nw")
+        self.password_label = ctk.CTkLabel(master=window, text='Password')
+        self.password_label.grid(row=3, column=0, padx=20, pady=5, sticky="nw")
 
-        self.password_entry = ctk.CTkEntry(master=self.tab("Create Staff"), textvariable=
+        self.password_entry = ctk.CTkEntry(master=window, textvariable=
                                        self.password, width=350)
         self.password_entry.grid(row=3, column=0, padx=(150,0), pady=5, sticky="w")
 
-        self.error_msg = ctk.CTkLabel(master=self.tab("Create Staff"), text="")
-        self.error_msg.grid(row=5, column=0, padx=(150,0), pady=(30,10), sticky="nw")
-        
+        self.role_label = ctk.CTkLabel(master=window, text='Role')
+        self.role_label.grid(row=4, column=0, padx=20, pady=10, sticky="nw")
+
         combobox_var = ctk.StringVar(value="Select Role")
-        self.drop = ctk.CTkComboBox(master=self.tab("Create Staff"), values=self.dropdown,
+        self.drop = ctk.CTkComboBox(master=window, values=self.dropdown,
                                     variable=combobox_var, command=self.combobox_callback,
                                     width=200, height=35, font=self.drop_font,
                                     fg_color="#f2f2f2", bg_color="#333333",
                                     text_color='black')
-        self.drop.grid(row=4, column=0, padx=(150,0), pady=25, sticky="nw")
+        self.drop.grid(row=4, column=0, padx=(150,0), pady=10, sticky="nw")
 
-        self.create_msg = ctk.CTkLabel(master=self.tab("Create Staff"), text="")
+        self.error_msg = ctk.CTkLabel(master=window, text="")
+        self.error_msg.grid(row=5, column=0, padx=(150,0), pady=(30,10), sticky="nw")
+
+        self.create_msg = ctk.CTkLabel(master=window, text="")
         self.create_msg.grid(row=5, column=0, padx=(150,0), pady=10, sticky="nw")
 
         self.create_frame = ctk.CTkFrame(master=self.tab("Create Staff"), fg_color="#333333")
-        self.create_frame.grid(row=6, column=0, sticky="sew", pady=(0,10), padx=10)
+        self.create_frame.grid(row=6, column=0, sticky="sew")
 
         self.create_button = ctk.CTkButton(master=self.create_frame,
                                 text='Create Staff',
                                 command=self.add_record)
-        self.create_button.grid(padx=10, row=0, column=0)
+        self.create_button.grid(pady=(0,10), padx=10, row=0, column=0)
 
         pywinstyles.set_opacity(self.drop, color="#333333")
 
@@ -246,53 +255,57 @@ class StaffView(ctk.CTkTabview):
         self.dropdown = ["Staff", "Frontend Staff", "Kitchen Staff",
                          "Chef", "Manager", "Admin"]
 
-        self.tab("Update Staff").columnconfigure((0, 1, 2), minsize=1000)
-        self.tab("Update Staff").rowconfigure(5, minsize=450)
+        self.tab("Update Staff").grid_rowconfigure(0, minsize=700)
 
-        self.label = ctk.CTkLabel(master=self.tab("Update Staff"), text="Update Staff Member",
+        window = ctk.CTkFrame(master=self.tab("Update Staff"),
+                              fg_color="#333333", height=600)
+        window.grid(row=0, column=0, sticky="n")
+        window.columnconfigure((0, 1, 2), minsize=1000)
+
+        self.label = ctk.CTkLabel(master=window, text="Update Staff Member",
                                   font=self.font)
         self.label.grid(row=0, column=0, padx=20, pady=10, sticky="w")
 
-        self.update_label = ctk.CTkLabel(master=self.tab("Update Staff"),
-                                      text="Please select a branch first")
+        self.update_label = ctk.CTkLabel(master=window,
+                                      text="Please select a staff member first")
         self.update_label.grid(row=1, column=0, padx=20, pady=5, sticky="w")
         
-        self.new_fullname_label = ctk.CTkLabel(master=self.tab("Update Staff"), text="New Full Name")
+        self.new_fullname_label = ctk.CTkLabel(master=window, text="New Full Name")
         self.new_fullname_label.grid(row=2, column=0, padx=20, pady=5, sticky="w")
 
-        self.new_fullname_entry = ctk.CTkEntry(master=self.tab("Update Staff"), textvariable=
+        self.new_fullname_entry = ctk.CTkEntry(master=window, textvariable=
                                        self.new_full_name, width=350)
-        self.new_fullname_entry.grid(row=2, column=0, padx=(160,0), pady=5, sticky="nw")
+        self.new_fullname_entry.grid(row=2, column=0, padx=(150,0), pady=5, sticky="nw")
 
-        self.new_role_label = ctk.CTkLabel(master=self.tab("Update Staff"), text="New Role")
-        self.new_role_label.grid(row=3, column=0, padx=20, pady=5, sticky="nw")
+        self.new_role_label = ctk.CTkLabel(master=window, text="New Role")
+        self.new_role_label.grid(row=3, column=0, padx=20, pady=10, sticky="nw")
 
         self.new_role = None
         combobox_var = ctk.StringVar(value="Select Role")
-        self.update_drop = ctk.CTkComboBox(master=self.tab("Update Staff"), values=self.dropdown,
+        self.update_drop = ctk.CTkComboBox(master=window, values=self.dropdown,
                                     variable=combobox_var, command=self.update_combobox_callback,
                                     width=200, height=35, font=self.drop_font,
                                     fg_color="#f2f2f2", bg_color="#333333",
                                     text_color='black')
-        self.update_drop.grid(row=3, column=0, padx=(160,0), pady=25, sticky="nw")
+        self.update_drop.grid(row=3, column=0, padx=(150,0), pady=10, sticky="nw")
 
-        self.update_msg = ctk.CTkLabel(master=self.tab("Update Staff"), text="")
-        self.update_msg.grid(row=4, column=0, padx=(160,0), pady=5, sticky="nw")
+        self.update_msg = ctk.CTkLabel(master=window, text="")
+        self.update_msg.grid(row=4, column=0, padx=(150,0), pady=5, sticky="nw")
+
+        self.update_error_msg = ctk.CTkLabel(master=window, text="")
+        self.update_error_msg.grid(row=4, column=0, padx=(150,0), pady=(30,10), sticky="nw")
 
         self.update_frame = ctk.CTkFrame(master=self.tab("Update Staff"), fg_color="#333333")
-        self.update_frame.grid(row=5, column=0, sticky="sew", pady=(0,10), padx=10)
+        self.update_frame.grid(row=5, column=0, sticky="sew")
 
         self.staff_update_btn = ctk.CTkButton(master=self.update_frame,
                                       text='Update Staff', command=lambda:
                                       self.update_record(self.updated_staff))
-        self.staff_update_btn.grid(padx=10, row=0, column=0)
+        self.staff_update_btn.grid(pady=(0,10), padx=10, row=0, column=0)
         
         self.staff_update_btn.configure(state="disabled")
         self.new_fullname_entry.configure(state="disabled")
         self.update_drop.configure(state="disabled")
-
-        self.update_error_msg = ctk.CTkLabel(master=self.tab("Update Staff"), text="")
-        self.update_error_msg.grid(row=5, column=0, padx=(160,0), pady=(30,10), sticky="nw")
 
     def update_combobox_callback(self, choice):
         self.new_role = self.configure_role_type(choice)
@@ -326,7 +339,7 @@ class StaffView(ctk.CTkTabview):
     def configure_update_widgets(self):
         self.new_full_name.set("")
         self.update_msg.configure(text="Staff member updated successfully")
-        self.update_label.configure(text="Please select a branch first")
+        self.update_label.configure(text="Please select a staff member first")
         self.staff_update_btn.configure(state="disabled")
         self.new_fullname_entry.configure(state="disabled")
         self.update_drop.configure(state="disabled")
@@ -334,7 +347,6 @@ class StaffView(ctk.CTkTabview):
         self.delete_button.configure(state="disabled")
 
     def configure_role_type(self, choice):
-        print(choice)
         self.role_id = None
         match choice:
             case "Admin":
